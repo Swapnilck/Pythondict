cc577f9b63b327bb30a8f3e2eb9c48267633d5a7
swapnilck <kulkarnisc.7350@gmail.com>
2025-09-15 16:49:29 +0530

    commit

--- mydictionaryprogram.py
+++ mydictionaryprogram.py
@@ -7,8 +7,10 @@ custdata:str= """CustomerID,Name,Email,Phone,Country
 4,Maria d,maria.d@example.com,555-4321,Spain
 5,Chen Wei,chen.wei@example.com,555-3456,China """
 
+
+
 #headerdata
-header=custdata.split("\n")[0]
+header=custdata.split("\n")[0].split(",")
 #print(header)
 #[CustomerID,Name,Email,Phone,Country]
 
@@ -34,10 +36,10 @@ nesteddict={}
 
 for key,value in newdict.items():
     nesteddict[key]={
-        "name" : value[0],
-        "mailid" : value[1],
-        "mobileno" : value[2],
-        "country" : value[3]
+        header[1] : value[0],
+        header[2] : value[1],
+        header[3] : value[2],
+        header[4] : value[3]
 
     }
 
@@ -45,9 +47,12 @@ for key,value in newdict.items():
 #print(nesteddict)
 #{'key': {'name': 'Chen Wei', 'mailid': 'chen.wei@example.com', 'mobileno': '555-3456', 'country': 'China '}}
 
-print(nesteddict.get("1").get("mobileno"))
+#print(nesteddict.get("1"))
 
+id=input("enter teh customer_id  :")
+details=input("enter what details u want  :")
 
+print(nesteddict.get(id).get(details))
 
 
 
