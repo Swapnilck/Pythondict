7aa1bf64d05652bd5f2fc64dded2fad8a43f3fd2
swapnilck <kulkarnisc.7350@gmail.com>
2025-09-16 09:30:50 +0530

    commit

--- set_datatype.py
+++ set_datatype.py
@@ -1,6 +1,12 @@
 #define set
-order_set={"man","wan","man"}
-print(order_set)
+#order_set={"man","wan","man"}
+#print(order_set)
 
 #op-{'man', 'wan'} --no duplicates allowed
 
+myset={1,2,3}
+myset.add(4)
+print(myset)    #op {1, 2, 3, 4}
+
+myset.remove(2)
+print(myset)      #op {1, 3, 4}
