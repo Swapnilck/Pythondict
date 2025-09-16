4d33cfc7c608af1f8807a5ba3a72e96cc29b536c
swapnilck <kulkarnisc.7350@gmail.com>
2025-09-16 09:40:43 +0530

    commit

--- set_datatype.py
+++ set_datatype.py
@@ -6,7 +6,18 @@
 
 myset={1,2,3}
 myset.add(4)
-print(myset)    #op {1, 2, 3, 4}
+#print(myset)    #op {1, 2, 3, 4}
 
 myset.remove(2)
-print(myset)      #opt {1, 3, 4}
+#print(myset)      #opt {1, 3, 4}
+
+
+op={(1,2,3),(1,2),(1,2,3)}
+op.remove((1,2))
+#print(op)
+
+#(km={[1,2,3],[1,2]}
+#print(km)        #TypeError: unhashable type: 'set'  ...primitive and tuples allowed in set
+
+jk={{1,3,3},{2,3,4}}
+print(jk)       #TypeError: unhashable type: 'set'
